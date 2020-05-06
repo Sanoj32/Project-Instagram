5fef03f8b0383c567beec4097212469813969ec1
Sanoj32 <59948661+Sanoj32@users.noreply.github.com>
2020-05-06 12:00:52 +0545

    image added on top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,79 +21,91 @@
     <link rel="shortcut icon" type="image/png"
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand d-flex pl-5" href=" {{ url('/') }}">
-                    <div><img src="/svg/instagramlogo.svg" style="height: 20px; border-right:1px solid #343a40" class="pr-3 pl-3 "></div>
-                    <div class="pl-3  pt-1">Instagram </div>
-                    <?php $idd = Auth::user()->id;
-                    $link='profile/'.$idd;
-                    ?>
-                    <div><a href={{ $link }} >{{ Auth::user()->username }}</div>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand d-flex pl-5" href=" {{ url('/') }}">
+                <div><img src="/svg/instagramlogo.svg" style="height: 20px; border-right:1px solid #343a40"
+                          class="pr-3 pl-3 "></div>
+                <div class="pl-3  pt-1">Instagram</div>
+                @auth
+                    <div class="d-flex justify-content-center">
+                        <?php
+                                                $idd = Auth::user()->id;
+                        $link = '/profile/' . $idd;
+                        ?>
+                        <div>  <img class="w-100 rounded-circle" src="{{Auth::user()->profile->profileImage()}}" style="max-width: 30px" alt=""></div>
+                            <div> <a href={{ $link }} >{{ Auth::user()->username }} </a> </div>
+                    </div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                @endauth
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->username }} <span class="caret"></span>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->username }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="row">
-                <div class="col-1"></div>
-                <div class="col-10">
-                    <main class="py-4">
-                        @yield('content')
-                    </main>
-                </div>
-                <div class="col-1"></div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                    @endguest
+                </ul>
             </div>
         </div>
-
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10">
+                <main class="py-4">
+                    @yield('content')
+                </main>
+            </div>
+            <div class="col-1"></div>
+        </div>
     </div>
+
+</div>
 
 </body>
 <div class="modal-footer d-flex justify-content-center">
-    <div>@2020 ProjectInsta </div>
-    <div> <a href="/about">About</a></div>
-    <div> laravel </div>
+    <div>@2020 ProjectInsta</div>
+    <div><a href="/about">About</a></div>
+    <div> laravel</div>
 </div>
 </html>
