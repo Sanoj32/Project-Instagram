c3d1d0283fa15e402c01cc014a248e47197f07a0
pravavlo <70977247+pravavlo@users.noreply.github.com>
2020-10-04 17:33:37 +0545

    Update app.js
    changed

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,11 +22,7 @@ window.Vue = require('vue');
 Vue.component('follow-button', require('./components/FollowButton.vue').default);
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
+ * huguuyvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvhhhhhhhhhhhhhhhhhhhhhhhhhhhh
 const app = new Vue({
     el: '#app',
 });
